cbb30800333f44e082754ac1fc8772e829bf5c6b
Dan Huynh <danhuynh@Dans-MacBook-Pro.local>
2026-02-09 15:14:28 -0500

    add in server and connection headers

--- webServer.py
+++ webServer.py
@@ -38,6 +38,8 @@ def webServer(port=13331):
       outputdata = b"HTTP/1.1 200 OK\r\n"      
       #Content-Type is an example on how to send a header as bytes. There are more!
       outputdata += b"Content-Type: text/html; charset=UTF-8\r\n"
+      outputdata += b"Server: dth6589 Python Server\r\n"
+      outputdata += b"Connection: close\r\n"
 
       #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
       outputdata += b"\r\n"
@@ -63,6 +65,8 @@ def webServer(port=13331):
       #Fill in start
       outputdata = b"HTTP/1.1 404 Not Found\r\n"
       outputdata += b"Content-Type: text/html; charset=UTF-8\r\n"
+      outputdata += b"Server: dth6589 Python Server\r\n"
+      outputdata += b"Connection: close\r\n"
       outputdata += b"\r\n"
       outputdata += b"<html><head></head><body><h1>404 Not Found</h1></body></html>"
       #Fill in end
